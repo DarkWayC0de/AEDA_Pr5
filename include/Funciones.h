deaf4f729ca2f49dcf3a72ca1ee489d3b2b1135f
DarkWayC0de <darkwayc0de@gmail.com>
2019-04-22 22:28:24 +0100

    Creacion de archivos nesesarios y ordenacion mergesort
    Se crean los archivos ordenacion.h, Funciones.h.
    En el cual en ordenacion se guardan los metodos de ordenacion
    y en funciones se guardan funciones auxiliares.
    Se crea el metodo de ordenacion mergesort.

---
+++ include/Funciones.h
@@ -0,0 +1,24 @@
+//
+// Created by darkwaycode on 9/04/19.
+//
+
+#ifndef PR5_FUNCIONES_H
+#define PR5_FUNCIONES_H
+
+#include <vector>
+
+template<class T>
+void ordenarv(std::vector<T> v1,int start1,int end1,std::vector<T> v2,int start2,int end2, std::vector<T> &v,int start,int end){
+  int j(start1),k(start2);
+  for (int i = start; i <=end ; ++i) {
+    if(((v1[j] < v2[k])||k==end2+1)&& (j<=end1)){
+      v[i] = v1[j];
+      j++;
+    } else {
+      v[i] = v2[k];
+      k++;
+    }
+  }
+}
+
+#endif //PR5_FUNCIONES_H
---
+++ include/ordenacion.h
@@ -0,0 +1,36 @@
+//
+// Created by darkwaycode on 9/04/19.
+//
+
+#ifndef PR5_ORDENACION_H
+#define PR5_ORDENACION_H
+
+#include <vector>
+#include "Funciones.h"
+
+template <class T>
+void Selec(std::vector<T> &v,int start,int end){
+  //TODO Implementar selec
+}
+template <class T>
+void Insert_Sac(std::vector<T> &v,int start,int end){
+  //TODO Implementar Insert_Sac
+}
+
+template <class T>
+void HeapSort(std::vector<T> &v, int start, int end){
+  //TODO Implementar HeatSort
+}
+
+template <class T>
+void MergeSort(std::vector<T> &v,int start, int end){
+  if(start < end ){
+    MergeSort(v, start, start+(end-start)/2);
+    MergeSort(v, (start+(end-start)/2)+1, end);
+    ordenarv<T>(v, start, start+(end-start)/2, v, (start+(end-start)/2)+1, end, v, start, end);
+  }
+}
+
+
+
+#endif //PR5_ORDENACION_H
